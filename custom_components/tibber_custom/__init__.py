c68fe86de22572b2efc9803481778e0f471716ea
Daniel Tedenljung <info@tedenljungconsulting.com>
2021-10-20 00:31:51 +0200

    Added config for dark mode

--- custom_components/tibber_custom/__init__.py
+++ custom_components/tibber_custom/__init__.py
@@ -1,11 +1,22 @@
 """Tibber custom"""
 import logging
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
 
 from homeassistant.const import EVENT_HOMEASSISTANT_START
 from homeassistant.helpers import discovery
 
 
 DOMAIN = "tibber_custom"
+
+CONF_USE_DARK_MODE = "use_dark_mode"
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_USE_DARK_MODE, default=False): cv.boolean,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
 
 DEPENDENCIES = ["tibber"]
 
@@ -14,6 +25,8 @@ _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
     """Setup component."""
+
+    use_dark_mode = config[DOMAIN][CONF_USE_DARK_MODE]
 
     def ha_started(_):
         discovery.load_platform(hass, "camera", DOMAIN, {}, config)
